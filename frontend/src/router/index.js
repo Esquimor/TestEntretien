c2a95201327a6eee7cb0d2dcc55557681480efa8
Adrien Guinet <adrien.guinet.ag@gmail.com>
2018-03-27 09:06:11 +0200

    Final

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,15 +1,22 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import UserRoute from '@/router/UserRoute'
+import Accueil from '@/page/Accueil'
 
 Vue.use(Router)
 
+// Route 1er niveau
+const routeDefault = [
+  {
+    path: '/',
+    name: 'HelloWorld',
+    component: Accueil
+  }
+]
+
+const routes = routeDefault.concat(UserRoute)
+
 export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
-  ]
+  mode: 'history',
+  routes
 })
